ee0a455b42511ce2e0cc817db7df7455ccc0db99
Bruno Krebs <bruno.krebs@auth0.com>
2017-09-07 11:36:00 -0300

    Taking advantage of coroutines from bluebird

--- auth0/login.js
+++ auth0/login.js
@@ -1,52 +1,40 @@
 function login(email, password, callback) {
     const request = require('request-promise@1.0.2');
+    const Promise = require('bluebird@3.4.6');
 
-    let userApp1;
-    let accessToken;
+    const authenticate = Promise.coroutine(function *() {
+        const getTokenOptions = {
+            method: 'POST',
+            url: 'https://bkrebs.auth0.com/oauth/token',
+            headers: {'content-type': 'application/json'},
+            body: '{"client_id":"3qu4Cxt4h2x9Em7Cj0s7Zg5FxhQLjiiK", "client_secret":"sUOIf4Psed68nU4hZvHlkRE2vCgUJF4UHlymKOJrgpn6oL8NJ3bOvdA1Y4ajo3IW","audience":"http://sample-app/","grant_type":"client_credentials"}'
+        };
 
-    const getTokenOptions = {
-        method: 'POST',
-        url: 'https://bkrebs.auth0.com/oauth/token',
-        headers: {'content-type': 'application/json'},
-        body: '{"client_id":"3qu4Cxt4h2x9Em7Cj0s7Zg5FxhQLjiiK","client_secret":"sUOIf4Psed68nU4hZvHlkRE2vCgUJF4UHlymKOJrgpn6oL8NJ3bOvdA1Y4ajo3IW","audience":"http://sample-app/","grant_type":"client_credentials"}'
-    };
+        // gets an access token to communicate with the legacy identity resources
+        const tokenResponse = yield request(getTokenOptions);
+        const accessToken = JSON.parse(tokenResponse).access_token;
 
-    request(getTokenOptions).then(function (body) {
+        //gets the user profile from the first application
+        const authApp1Response = yield legacyAuth(accessToken, 'http://node-app-1.tk/users/authenticate');
+        const profileApp1 = JSON.parse(authApp1Response);
 
-        accessToken = JSON.parse(body).access_token;
-        return authenticate('http://node-app-1.tk/users/authenticate');
+        //gets the user profile from the second application
+        const authApp2Response = yield legacyAuth(accessToken, 'http://node-app-2.tk/users/authenticate');
+        const profileApp2 = JSON.parse(authApp2Response);
 
-    }).then(function (body) {
+        // removes null properties from both profile to make merge (assign) unaware of them
+        Object.keys(profileApp1).forEach((key) => (profileApp1[key] === null) && delete profileApp1[key]);
+        Object.keys(profileApp2).forEach((key) => (profileApp2[key] === null) && delete profileApp2[key]);
 
-        userApp1 = JSON.parse(body);
-
-        return authenticate('http://node-app-2.tk/users/authenticate');
-
-    }).then(function (body) {
-
-        const userApp2 = JSON.parse(body);
-
-        Object.keys(userApp1).forEach((key) => (userApp1[key] === null) && delete userApp1[key]);
-        Object.keys(userApp2).forEach((key) => (userApp2[key] === null) && delete userApp2[key]);
-
-        const profile = Object.assign({}, userApp2, userApp1);
-        profile.email = email;
-
+        // merges both profiles
+        const profile = Object.assign({}, profileApp1, profileApp2, { email });
         return callback(null, profile);
-
-    }).catch(function (err) {
-
-        if (err.message) {
-            console.log(err.message);
-            return callback(new WrongUsernameOrPasswordError(email, err.message));
-        }
-
-        console.log(err);
-        return callback(new Error(err));
     });
 
-    function authenticate(url) {
-        const authApp1Options = {
+    authenticate();
+
+    function legacyAuth(accessToken, url) {
+        const options = {
             method: 'POST',
             url: url,
             headers: {
@@ -55,6 +43,6 @@ function login(email, password, callback) {
             },
             body: '{ "username": "' + email + '", "password": "' + password + '" }'
         };
-        return request(authApp1Options);
+        return request(options);
     }
 }
