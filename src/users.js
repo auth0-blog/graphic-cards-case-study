1344c21c5a00a3cb434c6c7ef76fe83515a9b6ce
Bruno Krebs <bruno.krebs@auth0.com>
2017-09-07 21:26:40 -0300

    Removing endpoint that returns users by id.

--- src/users.js
+++ src/users.js
@@ -8,18 +8,6 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get('/:id', (req, res) => {
-    const query = 'select * from users where id = ? limit 1';
-    const { id } = req.params;
-    mysql().query(query, [id]).then((users) => {
-        if (users.length === 0) {
-            res.status(404);
-            res.send({ message: 'User not found' });
-        }
-        res.send(users[0]);
-    });
-});
-
 router.get('/:email', (req, res) => {
     const query = 'select * from users where email = ? limit 1';
     const { email } = req.params;
