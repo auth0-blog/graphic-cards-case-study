b15ca3fb9cf0ff4bcf28f486a61d30e5aca6cfa1
Bruno Krebs <bruno.krebs@auth0.com>
2017-09-13 08:53:50 -0300

    refactoring users

--- db/node-1-users.sql
+++ db/node-1-users.sql
@@ -1,4 +1,3 @@
-insert into users (email, password, name, lastname, address, city, phone, gender) values
-  ('bruno@spam4.me', '123456', 'Bruno', null, null, 'New York', null, 'M'),
-  ('maria@spam4.me', '123456', 'Maria', 'da Silva', 'Avenue des Champs-Élysées', null, null, 'F'),
-  ('serena@spam4.me', '123456', 'Serena', 'Williams', null, 'New York', null, 'F');
+insert into users (email, password, name, lastname, address, city) values
+  ('bruno@spam4.me', '123456', 'Bruno', null, null, 'New York'),
+  ('serena@spam4.me', '123456', 'Serena', 'Williams', null, 'New York');
--- db/node-2-users.sql
+++ db/node-2-users.sql
@@ -1,4 +1,3 @@
-insert into users (email, password, name, lastname, address, city, phone, gender) values
-  ('bruno@spam4.me', '123456', 'Bruno', 'Krebs', '5th Avenue', null, null, 'M'),
-  ('maria@spam4.me', '123456', 'Maria', null, null, 'Paris', null, 'F'),
-  ('venus@spam4.me', '123456', 'Venus', 'Williams', null, 'New York', null, 'F');
+insert into users (email, password, name, lastname, address, city) values
+  ('bruno@spam4.me', '123456', 'Bruno', 'Krebs', '5th Avenue', null),
+  ('venus@spam4.me', '123456', 'Venus', 'Williams', null, 'New York');
